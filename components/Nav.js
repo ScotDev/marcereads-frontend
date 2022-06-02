adaadc5e9cf0fc9d74f3893482a7a6e8e8e75ee1
Callum <callum.git@outlook.com>
2022-06-02 20:59:44 +0100

    Finished mobile homepage minus bg graphics

--- components/Nav.js
+++ components/Nav.js
@@ -2,10 +2,14 @@ import navStyles from '../styles/Nav.module.css';
 import Link from "next/link";
 // https://youtu.be/mTz0GXj8NN0?t=1313
 
+import Button from './Button';
+
 import { FaInstagram } from 'react-icons/fa';
 
 
 export const Nav = () => {
+
+
     return (
         <nav className={navStyles.nav}>
             <div className={navStyles.desktop}>
@@ -27,10 +31,43 @@ export const Nav = () => {
                         <Link href="/"><a>About</a></Link>
                     </li>
                     <li>
-                        <Link href="/"><FaInstagram /></Link>
+                        <a href='https://www.instagram.com/marcereads/?hl=en' target="_blank" rel='noreferrer'><FaInstagram /></a>
                     </li>
                 </ul>
             </div>
+
+            <div className={navStyles.hamburger}>
+
+                <input type='checkbox'></input>
+                <div className={navStyles.hamburger_lines}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <ul>
+                    <li>
+                        <Link href="/"><a>Home</a></Link>
+                    </li>
+                    <li>
+                        <Link href="/"><a>Browse</a></Link>
+                    </li>
+                    <li>
+                        <Link href="/"><a>Reviews</a></Link>
+                    </li>
+                    <li>
+                        <Link href="/"><a>Guides</a></Link>
+                    </li>
+                    <li>
+                        <Link href="/"><a>About</a></Link>
+                    </li>
+                    <li>
+                        <Button text="@marcereads" href="https://www.instagram.com/marcereads/?hl=en" icon={<FaInstagram />} />
+                    </li>
+                </ul>
+            </div>
+            <p className={navStyles.brand_mobile}>Marcereads</p>
+            <a className={navStyles.instagram} href='https://www.instagram.com/marcereads/?hl=en' target="_blank" rel='noreferrer'><FaInstagram /></a>
         </nav>
     )
 }
