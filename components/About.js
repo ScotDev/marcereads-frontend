e2d20edeb03eb4905963a3c903085fe08c0cfc6e
Callum <callum.git@outlook.com>
2022-06-19 10:27:09 +0100

    Added data fetching to about, started on articles

--- components/About.js
+++ components/About.js
@@ -5,6 +5,7 @@ import AboutSectionStyles from '../styles/AboutSection.module.css';
 const TestAuthorjpeg = "https://i.imgur.com/fW0P7Wm.jpg";
 
 export default function About({ data }) {
+    console.log("About component data:", data)
     return (
         <section className={sectionStyles.section}>
             <div className={AboutSectionStyles.about}>
@@ -12,7 +13,7 @@ export default function About({ data }) {
                 <div className={AboutSectionStyles.image_wrapper}>
                     {/* <Image src={data.attributes.profile_image.data.attributes.url} objectFit="cover" alt="Portait of site's author" layout="fill" ></Image> */}
                 </div>
-                <p>{data.attributes.body}</p>
+                {/* <p>{data.attributes.body}</p> */}
             </div>
         </section>
     )
