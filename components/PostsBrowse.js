61f02cb2a4045ce1542cc1e0f9079dd81e8e87c4
Callum <callum.git@outlook.com>
2022-08-28 15:47:32 +0100

    Loadmore works on mobile too now :')

--- components/PostsBrowse.js
+++ components/PostsBrowse.js
@@ -17,111 +17,76 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 //  Similar load more function
 
 export default function PostsHomepage({ width, data }) {
-    // console.log(data)
     const isDesktop = width > 768 ? true : false;
 
 
     // console.log(finalData)
     let miniCardsData = [];
     const fullCardsData = useRef([]);
-    const dataToRenderRef = useRef([]);
     const counter = useRef(0);
     const [dataToRender, setdataToRender] = useState([]);
-    const maximum = Math.ceil(data.length / 6)
-
+    const [mincardDataToRender, setminicardDataToRender] = useState([]);
     const [loading, setloading] = useState(false);
 
 
     useEffect(() => {
 
         if (!isDesktop) {
+            setdataToRender(data[0].slice(0, 1))
             // fullCardsData.current = data.slice(0, 1)
             // setminiCardsData(data.slice(1, 6))
-            miniCardsData = data.flat().slice(1, 6)
+            setminicardDataToRender(data[0].slice(1, 6))
+            // miniCardsData = data.flat().slice(1, 6)
         } else if (isDesktop) {
-            // fullCardsData.current = data.slice(0, 6)
-
-
             setdataToRender(data[counter.current])
-            // dataToRenderRef.current = data[0]
-
-
-            // on first load
-            // for (let i = 0; i < data.length; i += chunkSize) {
-            //     const chunk = data.slice(i, i + chunkSize)
-            //     containerArray.current.push(chunk)
-            //     // console.log(containerArray.current)
-            //     // console.log(`chunk ${i}`, chunk)
-            // }
-            // fullCardsData.current = containerArray.current[0]
-
-            // for (let x = 0; x < data.length; x++) {
-            //     if (x < 6) {
-            //         fullCardsData.current.push(data[x])
-            //     }
-            // }
-            // data.forEach(item, index => {
-            //     if (index <= 6) {
-            //         miniCardsData.push(item)
-            //     }
-            // })
-            console.log(isDesktop)
-
         }
 
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
 
     const pushToArrayAndFlatten = async (val) => {
-        let tempArray = [...dataToRender];
+        let tempArray;
+
+        if (!isDesktop) {
+            tempArray = [...mincardDataToRender];
+        } else if (isDesktop) {
+            tempArray = [...dataToRender];
+        }
         tempArray.push(val);
         return tempArray.flat();
     }
 
 
     const loadMore = async () => {
+        setloading(true)
 
-        // for (let i = 0; i < data.length; i += chunkSize) {
-        //     const chunk = data.slice(i, i + chunkSize)
-        //     containerArray.current.push(chunk)
-        //     console.log(containerArray.current)
-        //     // console.log(`chunk ${i}`, chunk)
-        // }
-        // console.log(fullCardsData)
-
-        // increment counter ref, add that to state
-        if (counter.current < data.length - 1) {
-            counter.current += 1
-            const nextData = await pushToArrayAndFlatten(data[`${counter.current}`])
-            setdataToRender(nextData)
+        if (isDesktop) {
+            if (counter.current < data.length - 1) {
+                counter.current += 1
+                const nextData = await pushToArrayAndFlatten(data[`${counter.current}`])
+                setdataToRender(nextData)
+            }
+        } else if (!isDesktop) {
+            if (counter.current < data.length - 1) {
+                counter.current += 1
+                const nextData = await pushToArrayAndFlatten(data[`${counter.current}`])
+                setminicardDataToRender(nextData)
+            }
         }
 
-
-        // console.log(nextData)
-        // console.log(counter.current)
-
-
-
-        setloading(true)
         // Allow for loading animation and perceived feedback
         setTimeout(() => {
-            // setminiCardsData(data.slice(0, currentIndex))
-            // miniCardsData = data.slice(0, currentIndex)
-            // fullCardsData.push(data.slice(6, currentIndex).flat())
-            // setdataToRender(data.slice(0, currentIndexRef.current))
             setloading(false)
-            // console.log(currentIndexRef.current)
         }, 1000);
-        // console.log(currentIndex)
+        console.log("counter", counter.current)
     }
 
-    // const miniCards = dataToRender?.slice(0, currentIndexRef.current)?.map(item => {
     const Cards = dataToRender?.map(item => {
-        // const Cards = finalData?.map(item => {
         return <Card key={item.id} featured={item.attributes.isFeatured} url={`/articles/${item.id}`} type={item.attributes.type} title={item.attributes.title} author={item.attributes.author} date={item.attributes.date} imgURL={item.attributes.main.data.attributes.url} thumbnailURL={item.attributes.main.data.attributes.formats.thumbnail.url} />
     })
 
-    const miniCards = miniCardsData?.map(item => {
+    const miniCards = mincardDataToRender?.map(item => {
         return <MiniCard key={item.id} url={`/articles/${item.id}`} type={item.attributes.type} title={item.attributes.title} author={item.attributes.author} date={item.attributes.date} imgURL={item.attributes.main.data.attributes.url} thumbnailURL={item.attributes.main.data.attributes.formats.thumbnail.url} />
     })
 
@@ -130,8 +95,8 @@ export default function PostsHomepage({ width, data }) {
             <div className={cardGridStyles.grid}>
                 {Cards ? Cards : null}
                 {miniCards ? miniCards : null}
-                {loading ? <Loading /> : null}
-                {!loading ? <button onClick={() => loadMore()}>More</button> : null}
+                {/* {loading ? <Loading /> : null} */}
+                {!loading && counter.current < data.length - 1 ? <button onClick={() => loadMore()}>More</button> : null}
             </div>
 
         </section>
