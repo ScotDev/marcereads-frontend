661dfcfd65f110a18e58ee0e2ef2eeabe9cf8691
Callum <callum.git@outlook.com>
2022-09-06 15:51:24 +0100

    Added next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL,
+    siteUrl: process.env.NODE_ENV === "production" ? process.env.PROD_SITE_URL : process.env.DEV_SITE_URL,
     generateRobotsTxt: true,
 }
