5c10b05bb67aad53bc01065b6243b158554e3ae5
Callum <callum.git@outlook.com>
2022-09-06 21:46:09 +0100

    Testing On demand revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
     //     return res.status(401).json({ message: 'Invalid token' })
     // }
 
-    return res.json({ message: req.body })
 
     try {
         // this should be the actual path not a rewritten path
