b98bb4a3c78af6c950d604dab6fc7e8a858f4dd2
Callum <callum.git@outlook.com>
2022-09-06 21:07:42 +0100

    Testing On demand revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -10,12 +10,12 @@ export default async function handler(req, res) {
     try {
         // this should be the actual path not a rewritten path
         // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-        await res.revalidate(`/articles/47`)
+        await res.revalidate(`/articles`)
         return res.json({ revalidated: true })
     } catch (err) {
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
         // return res.status(500).send('Error revalidating')
-        return res.status(500).send(err)
+        return res.status(500).send(req.data)
     }
 }
