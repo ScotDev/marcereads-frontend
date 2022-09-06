c57b8232cbc746bbf59a9994b07dbc1db6727160
Callum <callum.git@outlook.com>
2022-09-06 20:48:27 +0100

    Testing On demand revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
     } catch (err) {
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
-        return res.status(500).send('Error revalidating')
+        // return res.status(500).send('Error revalidating')
+        return res.status(500).send(req.body.json())
     }
 }
